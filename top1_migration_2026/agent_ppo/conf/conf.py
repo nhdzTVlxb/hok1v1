52f696b592416e896f484c365361afbd5ae2041f
SpeedSci <181919026+nhdzTVlxb@users.noreply.github.com>
2026-05-08 01:28:55 +0800

    Update conf.py

--- top1_migration_2026/agent_ppo/conf/conf.py
+++ top1_migration_2026/agent_ppo/conf/conf.py
@@ -4,30 +4,30 @@
 # Copyright © 1998 - 2026 Tencent. All Rights Reserved.
 ###########################################################################
 """
-Top1-style PPO configuration adapted for the 2026 1v1 task.
+Compact PPO configuration for the first debuggable 2026 1v1 feature version.
 """
 
 
 class GameConfig:
     REWARD_WEIGHT_DICT = {
         "hp_point": 2.0,
-        "tower_hp_point": 8.0,
+        "tower_hp_point": 6.0,
         "money": 0.004,
-        "exp": 0.004,
-        "ep_rate": 0.75,
-        "death": -1.0,
-        "kill": -0.6,
-        "last_hit": 0.5,
-        "forward": 0.01,
+        "exp": 0.003,
+        "ep_rate": 0.1,
+        "death": -2.0,
+        "kill": 2.0,
+        "last_hit": 0.4,
+        "forward": 0.02,
+        "danger": -0.2,
+        "idle": -0.02,
     }
-    REMOVE_FORWARD_AFTER = 1000
+    REMOVE_FORWARD_AFTER = 1200
     TIME_SCALE_ARG = 8000
-    REWARD_WITHOUT_TIME_SCALE = set()
+    REWARD_WITHOUT_TIME_SCALE = {"death", "kill", "danger", "idle"}
     MODEL_SAVE_INTERVAL = 1800
     CAMP_HEROES = [112, 133]
 
-    # Deterministic summoner skill policy used by Agent.init_config.
-    # 80102 = Heal. Pair this with Agent's low-hp action override.
     HERO_SUMMONER_SKILL = {
         112: 80102,
         133: 80102,
@@ -38,93 +38,8 @@ class GameConfig:
     PASSIVE_BUTTONS = {0, 1, 7, 8, 9, 10, 11}
 
 
-class Args:
-    RELATIVE_DISTANCE_UNIT_SIZE = 600
-    RELATIVE_DISTANCE_MAX_SIZE = 24600
-    DIM_RELATIVE_DISTANCE = (RELATIVE_DISTANCE_MAX_SIZE // RELATIVE_DISTANCE_UNIT_SIZE + 2) * 2 + 1
-    WHOLE_DISTANCE_UNIT_SIZE = 5000
-    WHOLE_DISTANCE_MAX_SIZE = int(9e4)
-    DIM_WHOLE_DISTANCE = (WHOLE_DISTANCE_MAX_SIZE // WHOLE_DISTANCE_UNIT_SIZE) * 2 + 2
-    DIM_DISTANCE = DIM_RELATIVE_DISTANCE + DIM_WHOLE_DISTANCE
-
-    HP_UNIT_SIZE = 100
-    HP_MAX_SIZE = 3000
-
-    # 2026 heroes are Luban No.7 and Di Renjie. The exact 2026 buff/mark ids
-    # should still be debugged on-platform; these buckets cover the likely passive
-    # mark ids and reserve an unknown bucket instead of baking 2025 hero ids in.
-    MARK_ID_LAYERS = {
-        11200: 5,
-        13300: 5,
-    }
-    DIM_MARK = sum(v + 1 for v in MARK_ID_LAYERS.values()) + 1
-    DIM_UNIT = DIM_DISTANCE + HP_MAX_SIZE // HP_UNIT_SIZE + 3 + DIM_MARK
-
-    HERO_CONFIG_ID = [112, 133]
-    HERO_BEHAVE = [
-        "State_Dead",
-        "State_Idle",
-        "Direction_Move",
-        "Normal_Attack",
-        "State_Revive",
-        "UseSkill_1",
-        "UseSkill_2",
-        "UseSkill_3",
-    ]
-    EP_UNIT_SIZE = 30
-    EP_MAX_SIZE = 240
-    CD_UNIT_SIZE = 1000
-    CD_MAX_SIZE = 10000
-    LEVEL_MAX = 15
-    MONEY_UNIT_SIZE = 20
-    MONEY_MAX_SIZE = 300
-    COMMON_BUFFS = [90015, 10000, 10010, 11001, 11002, 11010, 11111]
-    HERO_BUFF_PREFIXES = (112, 133)
-    DIM_BUFF = len(COMMON_BUFFS) + 12 + 1
-
-    DIM_HERO = (
-        DIM_UNIT
-        + 1
-        + len(HERO_BEHAVE) + 1
-        + EP_MAX_SIZE // EP_UNIT_SIZE + 2
-        + (CD_MAX_SIZE // CD_UNIT_SIZE + 4) * 5
-        + LEVEL_MAX
-        + MONEY_MAX_SIZE // MONEY_UNIT_SIZE + 3
-        + 1
-        + 2
-        + DIM_BUFF
-    )
-
-    SOLDIER_MAX_NUM = 4
-    SOLDIER_BEHAVE = ["State_Dead", "Attack_Path"]
-    SOLDIER_CONFIG_ID = [[6801, 6804], [6800, 6803], [6802, 6805]]
-    DIM_SOLDIER = DIM_UNIT + len(SOLDIER_BEHAVE) + 1 + len(SOLDIER_CONFIG_ID) + 2
-    DIM_SOLDIERS = DIM_SOLDIER * SOLDIER_MAX_NUM
-
-    MONSTER_BEHAVE = ["State_Dead", "State_Auto", "State_Revive", "State_Born"]
-    DIM_MONSTER = DIM_UNIT + len(MONSTER_BEHAVE) + 1
-
-    DIM_ORGAN = DIM_UNIT + 3 + 2
-
-    BULLET_MAX_NUM = 10
-    BULLET_SLOT = [0, 1, 2, 3, "other"]
-    DIM_BULLET = len(BULLET_SLOT) + DIM_DISTANCE
-    DIM_BULLETS = DIM_BULLET * BULLET_MAX_NUM
-
-    DIM_ALL_UNITS = DIM_HERO * 2 + DIM_SOLDIERS * 2 + DIM_MONSTER + DIM_ORGAN * 2
-    DIM_ALL = DIM_ALL_UNITS + DIM_BULLETS
-
-
 class DimConfig:
-    DIM_OF_HERO_FRD = [Args.DIM_HERO]
-    DIM_OF_HERO_EMY = [Args.DIM_HERO]
-    DIM_OF_SOLDIER_1_4 = [Args.DIM_SOLDIER] * Args.SOLDIER_MAX_NUM
-    DIM_OF_SOLDIER_5_8 = [Args.DIM_SOLDIER] * Args.SOLDIER_MAX_NUM
-    DIM_OF_MONSTER = [Args.DIM_MONSTER]
-    DIM_OF_ORGAN_1 = [Args.DIM_ORGAN]
-    DIM_OF_ORGAN_2 = [Args.DIM_ORGAN]
-    DIM_OF_BULLET_1_9 = [Args.DIM_BULLET] * 9
-    DIM_OF_BULLET_10 = [Args.DIM_BULLET]
+    DIM_OF_FEATURE = [128]
 
 
 class Config:
@@ -132,10 +47,14 @@ class Config:
     LSTM_DROPOUT = 0
     LSTM_TIME_STEPS = 16
     LSTM_UNIT_SIZE = 512
-    DIM_PUBLIC = 512
+    DIM_PUBLIC = 256
+
+    FEATURE_DIM = DimConfig.DIM_OF_FEATURE[0]
+    LABEL_SIZE_LIST = [12, 16, 16, 16, 16, 9]
+    LEGAL_ACTION_FLAT_DIM = sum(LABEL_SIZE_LIST)
 
     DATA_SPLIT_SHAPE = [
-        Args.DIM_ALL + 85,
+        FEATURE_DIM + LEGAL_ACTION_FLAT_DIM,
         1,
         1,
         1,
@@ -160,13 +79,13 @@ class Config:
         LSTM_UNIT_SIZE,
         LSTM_UNIT_SIZE,
     ]
-    SERI_VEC_SPLIT_SHAPE = [(Args.DIM_ALL,), (85,)]
+    SERI_VEC_SPLIT_SHAPE = [(FEATURE_DIM,), (LEGAL_ACTION_FLAT_DIM,)]
+
     INIT_LEARNING_RATE_START = 1e-4
     TARGET_LR = 1e-5
     TARGET_STEP = 10000
-    BETA_START = 0.0
+    BETA_START = 0.01
     LOG_EPSILON = 1e-6
-    LABEL_SIZE_LIST = [12, 16, 16, 16, 16, 9]
     IS_REINFORCE_TASK_LIST = [True, True, True, True, True, True]
 
     CLIP_PARAM = 0.2
@@ -174,30 +93,30 @@ class Config:
     TARGET_EMBED_DIM = 32
 
     data_shapes = [
-        [(Args.DIM_ALL + 85) * 16],
-        [16],
-        [16],
-        [16],
-        [16],
-        [16],
-        [16],
-        [16],
-        [16],
-        [192],
-        [256],
-        [256],
-        [256],
-        [256],
-        [144],
-        [16],
-        [16],
-        [16],
-        [16],
-        [16],
-        [16],
-        [16],
-        [512],
-        [512],
+        [(FEATURE_DIM + LEGAL_ACTION_FLAT_DIM) * LSTM_TIME_STEPS],
+        [LSTM_TIME_STEPS],
+        [LSTM_TIME_STEPS],
+        [LSTM_TIME_STEPS],
+        [LSTM_TIME_STEPS],
+        [LSTM_TIME_STEPS],
+        [LSTM_TIME_STEPS],
+        [LSTM_TIME_STEPS],
+        [LSTM_TIME_STEPS],
+        [12 * LSTM_TIME_STEPS],
+        [16 * LSTM_TIME_STEPS],
+        [16 * LSTM_TIME_STEPS],
+        [16 * LSTM_TIME_STEPS],
+        [16 * LSTM_TIME_STEPS],
+        [9 * LSTM_TIME_STEPS],
+        [LSTM_TIME_STEPS],
+        [LSTM_TIME_STEPS],
+        [LSTM_TIME_STEPS],
+        [LSTM_TIME_STEPS],
+        [LSTM_TIME_STEPS],
+        [LSTM_TIME_STEPS],
+        [LSTM_TIME_STEPS],
+        [LSTM_UNIT_SIZE],
+        [LSTM_UNIT_SIZE],
     ]
 
     LEGAL_ACTION_SIZE_LIST = LABEL_SIZE_LIST.copy()
