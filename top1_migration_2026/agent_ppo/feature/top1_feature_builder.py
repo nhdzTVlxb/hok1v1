c71e55f9ff12b94457d40943015bcdeb4f4d5ca1
SpeedSci <181919026+nhdzTVlxb@users.noreply.github.com>
2026-05-07 22:11:23 +0800

    Update top1_feature_builder.py

--- top1_migration_2026/agent_ppo/feature/top1_feature_builder.py
+++ top1_migration_2026/agent_ppo/feature/top1_feature_builder.py
@@ -30,11 +30,11 @@ def get_any(d, *keys, default=None):
 
 def camp_id(camp):
     if isinstance(camp, str):
-        if camp[-1:].isdigit():
-            return int(camp[-1])
         if camp in ("0", "1", "2"):
             value = int(camp)
             return value + 1 if value in (0, 1) else value
+        if camp[-1:].isdigit():
+            return int(camp[-1])
     if isinstance(camp, int):
         return camp + 1 if camp in (0, 1) else camp
     return camp
