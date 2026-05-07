fad6e8683fe2602c580a681d23a046612fac475f
SpeedSci <181919026+nhdzTVlxb@users.noreply.github.com>
2026-05-08 03:44:20 +0800

    Update monitor_builder.py

--- top1_migration_2026/agent_ppo/conf/monitor_builder.py
+++ top1_migration_2026/agent_ppo/conf/monitor_builder.py
@@ -74,6 +74,57 @@ def build_monitor():
             expr="round(avg(entropy_loss{}), 0.01)",
         )
         .end_panel()
+        .add_panel(
+            name="英雄塔伤",
+            name_en="hero_tower_damage",
+            type="line",
+        )
+        .add_metric(metrics_name="diy_7", expr="round(avg(diy_7{}), 0.01)")
+        .end_panel()
+        .add_panel(
+            name="总塔伤",
+            name_en="tower_damage",
+            type="line",
+        )
+        .add_metric(metrics_name="diy_6", expr="round(avg(diy_6{}), 0.01)")
+        .end_panel()
+        .add_panel(
+            name="安全推塔帧",
+            name_en="safe_push_frames",
+            type="line",
+        )
+        .add_metric(metrics_name="diy_8", expr="round(avg(diy_8{}), 0.01)")
+        .add_metric(metrics_name="diy_9", expr="round(avg(diy_9{}), 0.01)")
+        .end_panel()
+        .add_panel(
+            name="攻击频率",
+            name_en="attack_frequency",
+            type="line",
+        )
+        .add_metric(metrics_name="diy_13", expr="round(avg(diy_13{}), 0.01)")
+        .add_metric(metrics_name="diy_14", expr="round(avg(diy_14{}), 0.01)")
+        .add_metric(metrics_name="diy_15", expr="round(avg(diy_15{}), 0.01)")
+        .end_panel()
+        .add_panel(
+            name="攻击属性",
+            name_en="attack_stats",
+            type="line",
+        )
+        .add_metric(metrics_name="diy_11", expr="round(avg(diy_11{}), 0.01)")
+        .add_metric(metrics_name="diy_12", expr="round(avg(diy_12{}), 0.01)")
+        .add_metric(metrics_name="diy_16", expr="round(avg(diy_16{}), 0.01)")
+        .add_metric(metrics_name="diy_17", expr="round(avg(diy_17{}), 0.01)")
+        .end_panel()
+        .add_panel(
+            name="打塔动作",
+            name_en="tower_action",
+            type="line",
+        )
+        .add_metric(metrics_name="diy_10", expr="round(avg(diy_10{}), 0.01)")
+        .add_metric(metrics_name="diy_18", expr="round(avg(diy_18{}), 0.01)")
+        .add_metric(metrics_name="diy_19", expr="round(avg(diy_19{}), 0.01)")
+        .add_metric(metrics_name="diy_20", expr="round(avg(diy_20{}), 0.01)")
+        .end_panel()
         .end_group()
         .build()
     )
